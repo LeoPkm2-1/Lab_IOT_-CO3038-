b7112f13a46e19ffccca2fa111a49ad768cd469f
leo <thinhphatmai2001@gmail.com>
2024-05-09 17:28:24 +0700

    .

--- IOT_LAB_FOR_WINDOWNS/MyIOTAndroiApp/app/src/main/java/com/example/myiotandroiapp/MQTTHelper.java
+++ IOT_LAB_FOR_WINDOWNS/MyIOTAndroiApp/app/src/main/java/com/example/myiotandroiapp/MQTTHelper.java
@@ -18,16 +18,15 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 public class MQTTHelper {
     public MqttAndroidClient mqttAndroidClient;
 
-    //    public final String[] arrayTopics = {"thinhphatmai2001/feeds/cambien1", "thinhphatmai2001/feeds/cambien2", "thinhphatmai2001/feeds/cambien3",
-//            "thinhphatmai2001/feeds/nutnhan1","thinhphatmai2001/feeds/nutnhan2"};
-    public final String[] arrayTopics = {".......................", ".............................", "......................................",
-            ".........................",".........................."};
+    public final String[] arrayTopics = {"thinhphatmai2001/feeds/cambien1", "thinhphatmai2001/feeds/cambien2", "thinhphatmai2001/feeds/cambien3",
+            "thinhphatmai2001/feeds/nutnhan1","thinhphatmai2001/feeds/nutnhan2","thinhphatmai2001/feeds/nutnhan3"};
+
 
     final String clientId = "123456879";
-    final String username = "................................";
-    final String password = "............................";
+    final String username = "thinhphatmai2001";
+    final String password = "aio_WcWe35j3BHUjPqymihvR60ikRUcT";
 
-    final String serverUri = "..............................";
+    final String serverUri = "tcp://io.adafruit.com:1883";
 
     public MQTTHelper(Context context){
         mqttAndroidClient = new MqttAndroidClient(context, serverUri, clientId);
--- IOT_LAB_FOR_WINDOWNS/MyIOTAndroiApp/app/src/main/java/com/example/myiotandroiapp/MainActivity.java
+++ IOT_LAB_FOR_WINDOWNS/MyIOTAndroiApp/app/src/main/java/com/example/myiotandroiapp/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.graphics.Color;
+
 
 import com.github.angads25.toggle.interfaces.OnToggledListener;
 import com.github.angads25.toggle.model.ToggleableView;
@@ -20,8 +22,8 @@ import java.nio.charset.Charset;
 public class MainActivity extends AppCompatActivity {
 
     MQTTHelper mqttHelper;
-    TextView txtTemp, txtHumi;
-    LabeledSwitch btnLED,btnPUMP;
+    TextView txtTemp, txtHumi,txtBright;
+    LabeledSwitch btnLED,btnPUMP,btnDOOR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         txtHumi = findViewById(R.id.txtHumidity);
         btnLED = findViewById(R.id.btnLED);
         btnPUMP = findViewById(R.id.btnPUMP);
+        txtBright = findViewById(R.id.txtBrightness);
+        btnDOOR = findViewById(R.id.btnDOOR);
 
         btnLED.setOnToggledListener(new OnToggledListener() {
             @Override
@@ -50,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
                     sendDataMQTT("thinhphatmai2001/feeds/nutnhan2","1");
                 }else{
                     sendDataMQTT("thinhphatmai2001/feeds/nutnhan2","0");
+                }
+            }
+        });
+        btnDOOR.setOnToggledListener(new OnToggledListener() {
+            @Override
+            public void onSwitched(ToggleableView toggleableView, boolean isOn) {
+                if(isOn == true){
+                    sendDataMQTT("thinhphatmai2001/feeds/nutnhan3","1");
+                }else{
+                    sendDataMQTT("thinhphatmai2001/feeds/nutnhan3","0");
                 }
             }
         });
@@ -89,9 +103,70 @@ public class MainActivity extends AppCompatActivity {
             public void messageArrived(String topic, MqttMessage message) throws Exception {
                 Log.d("TEST", topic + " --- " + message.toString());
                 if(topic.contains("cambien1")){
-                    txtTemp.setText(message.toString()+"°C");
-                } else if(topic.contains("cambien2")){
-                    txtHumi.setText(message.toString()+"%");
+                    String cambien1_data_str = message.toString();
+                    float cambien1_data = Float.parseFloat(cambien1_data_str);
+                    txtTemp.setText(cambien1_data_str+"°C");
+                    if(cambien1_data<10){
+                        String hexColor = "#2b80b4"; // Example color code (gold color)
+                        int color = Color.parseColor(hexColor);
+                        txtTemp.setBackgroundColor(color);
+                    }else if(cambien1_data<30){
+                        String hexColor = "#01afd7"; // Example color code (gold color)
+                        int color = Color.parseColor(hexColor);
+                        txtTemp.setBackgroundColor(color);
+                    }else{
+                        String hexColor = "#f35757"; // Example color code (gold color)
+                        int color = Color.parseColor(hexColor);
+                        txtTemp.setBackgroundColor(color);
+                    }
+
+
+                } else if(topic.contains("cambien3")) {
+
+                    String cambien3_data_str = message.toString();
+                    float cambien3_data = Float.parseFloat(cambien3_data_str);
+                    txtHumi.setText(cambien3_data_str + "%");
+                     if(cambien3_data<60){
+                        String hexColor = "#18469e"; // Example color code (gold color)
+                        int color = Color.parseColor(hexColor);
+                        txtHumi.setBackgroundColor(color);
+                        // open pump
+                         btnPUMP.setOn(true);
+                         sendDataMQTT("thinhphatmai2001/feeds/nutnhan2","1");
+
+                    }else {
+                        String hexColor = "#01afd7"; // Example color code (gold color)
+                        int color = Color.parseColor(hexColor);
+                        txtHumi.setBackgroundColor(color);
+                         // close pump
+                         btnPUMP.setOn(false);
+                         sendDataMQTT("thinhphatmai2001/feeds/nutnhan2","0");
+                    }
+                }else if(topic.contains("cambien2")){
+                    String cambien2_data_str = message.toString();
+                    float cambien2_data = Float.parseFloat(cambien2_data_str);
+                    txtBright.setText(cambien2_data +"-LUX");
+                    if(cambien2_data<50){
+
+                        String hexColor = "#705b0a"; // Example color code (gold color)
+                        int color = Color.parseColor(hexColor);
+                        txtBright.setBackgroundColor(color);
+
+                        // On led
+                        btnLED.setOn(true);
+                        sendDataMQTT("thinhphatmai2001/feeds/nutnhan1","1");
+                    }else if(cambien2_data<400){
+                        String hexColor = "#fcbf4a"; // Example color code (gold color)
+                        int color = Color.parseColor(hexColor);
+                        txtBright.setBackgroundColor(color);
+                    }else{
+                        String hexColor = "#f7f2c7"; // Example color code (gold color)
+                        int color = Color.parseColor(hexColor);
+                        txtBright.setBackgroundColor(color);
+                        // Off led
+                        btnLED.setOn(false);
+                        sendDataMQTT("thinhphatmai2001/feeds/nutnhan1","0");
+                    }
                 }else if(topic.contains("nutnhan1")){
                     if(message.toString().equals("1")){
                         btnLED.setOn(true);
@@ -103,6 +178,12 @@ public class MainActivity extends AppCompatActivity {
                         btnPUMP.setOn(true);
                     }else{
                         btnPUMP.setOn(false);
+                    }
+                }else if(topic.contains("nutnhan3")){
+                    if(message.toString().equals("1")){
+                        btnDOOR.setOn(true);
+                    }else{
+                        btnDOOR.setOn(false);
                     }
                 }
             }
